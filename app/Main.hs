c65155a6930e91921e6a948e4bffeff1c8b26ea3
Goju Ryu <mads1606@gmail.com>
2020-10-27 13:46:12 +0100

    Game is now playable! Still lacks UX and a computer opponent.

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,27 @@ module Main where
 import Game
 import Lib
 
-main :: IO ()
-main = do 
+
+userIO :: GameState -> IO String
+userIO s = do 
     { putStrLn "Board:"
-    ; putStrLn $ show makeNewGame ++ "\n"
+    ; putStrLn $ show s ++ "\n"
     ; putStrLn "Make move: " 
-    ; inStr <- getLine
-    ; putStrLn inStr
+    ; getLine  -- This value is returned
     }
+
+readMove :: String -> Move
+readMove str = read str
+
+gameLoop :: GameState -> Player -> IO b
+gameLoop s p = do 
+    { inStr <- userIO s
+    ; let move = readMove inStr
+    ; let result = makeMove p move s
+    ; case (result) of 
+    ;    Left newState -> gameLoop  newState $ swichPlayer p
+    ;    Right (msg, oldState) -> putStrLn msg >> gameLoop oldState p
+    } 
+
+main :: IO ()
+main = gameLoop makeNewGame PlayerX
--- src/Game.hs
+++ src/Game.hs
@@ -1,7 +1,10 @@
 module Game
   ( GameState
-  , Player
+  , Player(..)
+  , Move(..)
   , makeNewGame
+  , makeMove
+  , swichPlayer
   )
 where
 
@@ -10,19 +13,36 @@ data GameState = State
   , r2 :: (Maybe Player, Maybe Player, Maybe Player)
   , r3 :: (Maybe Player, Maybe Player, Maybe Player)
   }
-
 instance Show GameState where
   show (State r1 r2 r3) = show r1 ++ "\n" ++ show r2 ++ "\n" ++ show r3
 
 
-data Player = PlayerO | PlayerX deriving (Show, Eq)
+data Player = PlayerO | PlayerX deriving (Eq)
+instance Show Player where
+  show PlayerO = "O"
+  show PlayerX = "X"
 
-data Move = TopL | TopM | TopR | MidL | MidM | MidR | BottomL | BottomM |BottomR
+data Move = TopL | TopM | TopR | MidL | MidM | MidR | BottomL | BottomM | BottomR deriving (Eq, Read)
 
 
 
 makeNewGame :: GameState
 makeNewGame = State (Nothing, Nothing, Nothing) (Nothing, Nothing, Nothing) (Nothing, Nothing, Nothing) 
 
-makeMove :: GameState -> Player -> Move -> GameState
-makeMove _ _ _ = makeNewGame 
+swichPlayer :: Player -> Player
+swichPlayer p  
+    | p == PlayerO = PlayerX
+    | p == PlayerX = PlayerO
+
+makeMove ::  Player -> Move -> GameState -> Either GameState (String, GameState)
+makeMove p m s= case (m, s)  of
+  (TopL, (State (Nothing, r1m, r1r) r2 r3)) -> Left $ State (Just p, r1m, r1r) r2 r3
+  (TopM, (State (r1l, Nothing, r1r) r2 r3)) -> Left $ State (r1l, Just p, r1r) r2 r3
+  (TopR, (State (r1l, r1m, Nothing) r2 r3)) -> Left $ State (r1l, r1m, Just p) r2 r3
+  (MidL, (State r1 (Nothing, r1m, r1r) r3)) -> Left $ State r1 (Just p, r1m, r1r) r3
+  (MidM, (State r1 (r1l, Nothing, r1r) r3)) -> Left $ State r1 (r1l, Just p, r1r) r3
+  (MidR, (State r1 (r1l, r1m, Nothing) r3)) -> Left $ State r1 (r1l, r1m, Just p) r3
+  (BottomL, (State r1 r2 (Nothing, r1m, r1r))) -> Left $ State r1 r2 (Just p, r1m, r1r)
+  (BottomM, (State r1 r2 (r1l, Nothing, r1r))) -> Left $ State r1 r2 (r1l, Just p, r1r)
+  (BottomR, (State r1 r2 (r1l, r1m, Nothing))) -> Left $ State r1 r2 (r1l, r1m, Just p)
+  (_, _)-> Right ("Cannot take already occupied spot!", s)
